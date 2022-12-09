5bc39c8f3e2b1f338f029502fe8be90a93b8bc8c
Mechak BITI <mechakbiti33@gmail.com>
2022-12-09 10:12:15 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 
 /**
  * main - Entry point
@@ -11,10 +10,12 @@
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 	{	
 		putchar(low);
 	}
 	putchar('\n');
+
 	return (0);
 }
